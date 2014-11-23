b4a250164213a098f128a4584ddf06f0f4036f75
Brent Dimmig <brentdimmig@gmail.com>
2014-11-23 15:19:08 -0500

    Updated visualize bytes to include colorbar and class labels.

--- visualize/visualizeBytes.m
+++ visualize/visualizeBytes.m
@@ -5,19 +5,23 @@ function visualizeBytes( Hbytes )
 % Everything red
 R = linspace(0.4,0.8,255);
 G = linspace(0.0,0.0,255);
-B = linspace(0.0,0.8,255);
+B = linspace(0.4,0.8,255);
 cmap = [R' G' B'];
 % Black for zero
 cmap(1,:) = [0 0 0];
 % White for 255
 cmap(end,:) = [1 1 1];
 R = linspace(0.7,1.0,96);
-G = linspace(0.7,0.7,96);
+G = linspace(0.7,1.0,96);
 B = linspace(0.0,0.0,96);
 cmap(32:127,:) = [R' G' B'];
 colormap(cmap);
 image(Hbytes);
 axis square
+colorbar;
+hcb=colorbar;
+set(hcb,'YTick',[1, 80, 180, 255])
+set(hcb,'YTickLabel',{'Null','Printable Chars.','Other','255'});
 
 end
 
